a274f338dea51d03b9daf2f9051336eba1f9931e
choisungwook <kgg1959@naver.com>
2020-09-29 00:05:46 +0900

    Add findAll BoardEntity Test

--- src/test/java/com/board/board/service/BoardServiceTest.java
+++ src/test/java/com/board/board/service/BoardServiceTest.java
@@ -1,12 +1,15 @@
 package com.board.board.service;
 
-import com.board.board.domain.Board;
+import com.board.board.domain.board.Board;
+import com.board.board.domain.board.Dto.BoardResponseFindAllDto;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -16,17 +19,36 @@ public class BoardServiceTest {
 
     @Test
     public void 게시판생성(){
-        //given
-        Board board = Board.builder()
-                .title("테스트 게시판")
-                .build();
+        create_board("테스트 게시판");
+    }
 
-        //when
+    @Test
+    public void 게시판다수_조회(){
+        for(int i=0; i<=100; i++){
+            String title = "테스트 게시판" + Integer.toString(i);
+            create_board(title);
+        }
+
+        List<BoardResponseFindAllDto> all_to_dto = boardService.findAll_to_Dto();
+
+        all_to_dto.forEach(dto -> System.out.println(dto.getId() + ", " + dto.getTitle()));
+    }
+
+    @Test
+    public void 중복생성_테스트(){
+
+    }
+
+    private Board create_board(String title){
+        Board board = Board.builder()
+                .title(title)
+                .build();
         Long saveId = boardService.save(board);
 
-        //then
+        //검증
         Board findBoard = boardService.findById(saveId);
-
         Assertions.assertThat(findBoard.getTitle()).isEqualTo(board.getTitle());
+
+        return board;
     }
 }
