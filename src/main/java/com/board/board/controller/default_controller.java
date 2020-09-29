2f7cc56701a60a3127917f850b12c49b43817245
choisungwook <kgg1959@naver.com>
2020-09-29 09:30:09 +0900

    Add 왼쪽 메뉴 Controller, view

--- src/main/java/com/board/board/controller/default_controller.java
+++ src/main/java/com/board/board/controller/default_controller.java
@@ -1,13 +1,23 @@
 package com.board.board.controller;
 
+import com.board.board.domain.board.Dto.BoardResponseFindAllDto;
+import com.board.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class default_controller {
+    private final BoardService boardService;
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        List<BoardResponseFindAllDto> boards = boardService.findAll_to_Dto();
+        model.addAttribute("boards", boards);
         return "index";
     }
 }
